30310c2c7b3f8bf4f1871479b4aae01f33e60ffe
Sam Stokes <me@samstokes.co.uk>
2024-12-27 23:46:17 -0800

    cache todo -c output for 10s and shutdown old jobs

--- lua/telescope-todo/init.lua
+++ lua/telescope-todo/init.lua
@@ -4,9 +4,22 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local conf = require("telescope.config").values
 
+-- Store the job and cache state
+local M = {
+	_current_job = nil,
+	_cached_output = "",
+	_cache_timestamp = 0,
+	_cache_duration = 10000, -- Cache duration in ms
+}
+
 local setup = function(opts)
 	require("telescope").load_extension("todo")
 	require("plenary")
+
+	-- Allow configuring cache duration
+	if opts and opts.cache_duration then
+		M._cache_duration = opts.cache_duration
+	end
 end
 
 local todo_picker = function(opts)
@@ -28,8 +41,6 @@ local todo_picker = function(opts)
 		:find()
 end
 
-local _todo_output = "0 TODO"
-
 local handle_todo_count = function(output)
 	if output == "0 TODO" then
 		return ""
@@ -38,20 +49,40 @@ local handle_todo_count = function(output)
 	end
 end
 
-local todo_count = function(opts)
+local todo_count = function()
+	---@diagnostic disable-next-line: undefined-field
+	local current_time = vim.loop.now()
+
+	-- Return cached result if it's still fresh
+	if current_time - M._cache_timestamp < M._cache_duration then
+		return handle_todo_count(M._cached_output)
+	end
+
+	-- Cancel any existing job
+	if M._current_job and not M._current_job.is_shutdown then
+		M._current_job:shutdown()
+	end
+
+	-- Create new job
 	local Job = require("plenary.job")
 	---@diagnostic disable-next-line: missing-fields
-	Job:new({
+	M._current_job = Job:new({
 		command = "todo",
 		args = { "-c" },
 		on_exit = function(j, return_val)
 			if return_val == 0 then
-				_todo_output = j:result()[1]
+				M._cached_output = j:result()[1]
+				M._cache_timestamp = current_time
 			end
 		end,
-	}):start()
+	})
 
-	return handle_todo_count(_todo_output)
+	M._current_job:start()
+	return handle_todo_count(M._cached_output)
 end
 
-return { setup = setup, todo_picker = todo_picker, todo_count = todo_count }
+return {
+	setup = setup,
+	todo_picker = todo_picker,
+	todo_count = todo_count,
+}
